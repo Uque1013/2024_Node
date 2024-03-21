46e9c6765206c8a3596d475f86482565c4adb6ee
kye <kye6735@naver.com>
2024-03-21 15:26:03 +0900

    동등비교

---
+++ Grammar/07.equality.js
@@ -0,0 +1,11 @@
+// 값만 비교
+console.log(1 == 1) // true
+console.log(1 == '1') // true
+console.log(0 == false) // true
+console.log(0 == '') // true
+
+// 값 + 자료형 비교
+console.log(1 === 1) // true
+console.log(1 === '1') // false
+console.log(0 === false) // false
+console.log(0 === '') // false
