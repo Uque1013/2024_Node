0149acc04f082f751697c53f465a3b63b07e9220
kye <kye6735@naver.com>
2024-04-01 11:17:17 +0900

    express static

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
+const path = require('path')    // node에서 기본으로 제공하는 패키지
 const app = express()
 const port = 3000
+
+app.use('/html', express.static(path.join(__dirname), 'html'))
 
 // get method 요청에 대한 라우팅 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.POST('/cute', (req, res) => {
-    res.send('cute jisu')
-})
-
 app.listen(port, () =>{
     console.log(`listening on port ${port}`)
+    console.log(express.static(path.join(__dirname), 'html'))
 })
