58008cbefdc1a5ee676a6646a137887832947d4a
kye <kye6735@naver.com>
2024-04-15 10:45:59 +0900

    상태저장 체이닝

--- middleware.js
+++ middleware.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const middle1 = require('./my_modules/middle1')
+
 
 // 미들웨어 등록 use:모든 method(get, post, put, delete등등)에 대해 처리
 // arg로 경로를 지정하지 않으면, 모든 경로에 대해 처리
 app.use(middle1)
+app.use(morgan)
 
 app.get('/', (req, res, next)=> {
     // 애플리케이션 수준의 상태 저장
